9b10989a00119c0eb613b7d0ad2c19ad8e27f206
Smurvy <75571383+Smurvy@users.noreply.github.com>
2022-01-03 09:58:55 -0500

    Update passwordGenerator.py
    Made the code for choosing what list smaller

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,25 +1,11 @@
 from random import *
 
-specialChar = ["!","@","#", "$", "%", "^", "&", "*",]
-
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
+allChar = ["!","@","#", "$", "%", "^", "&", "*","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 passLength = int(input("How long would you like your password to be? >> "))
 
 password = []
 
 for x in range(passLength):
-    typeChoice = randint(1,3)
-
-    if typeChoice == 1:
-        password.append(specialChar[randint(0,len(specialChar) + 1)])
-
-    elif typeChoice == 2:
-        password.append(alphabet[randint(0,len(alphabet) + 1)])
-
-    elif typeChoice == 3:
-        password.append(num[randint(0,len(num) + 1)])
-
+    password.append(allChar[randint(0,len(allChar))])
+    
 print("Your new password is: " + "".join(password))
